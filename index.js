06890d3d75b861a1cbda5e8ac066b3f47168589c
kduckets <kmditroia@gmail.com>
2017-09-24 11:31:33 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ server.route({
     if (!request.payload.list_id) return reply({ detail: "no_list_id" }).code(400);
     mailchimp.post(`/lists/${request.payload.list_id}/members`, {
       email_address: request.payload.email,
-      merge_fields: {'RSVP':request.payload.RSVP, 'NAME':request.payload.NAME}},
+      merge_fields: {'RSVP':request.payload.RSVP, 'NAME':request.payload.NAME},
       status: 'subscribed'
     }, function(err, results) {
       if (err) {
