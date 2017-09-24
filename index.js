ea0dff63835742b5241ceaf4b21356f92ec639a0
kduckets <kmditroia@gmail.com>
2017-09-24 12:55:24 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ server.route({
   method: 'POST',
   path: '/',
   handler: function (request, reply) {
-    var fields = {};
+    var fields = [];
     fields["RSVP"] = request.payload.RSVP;
     fields["NAME"] = request.payload.NAME;
     console.log(JSON.stringify(fields));
