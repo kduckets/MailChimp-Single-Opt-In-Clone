49e9262b9e8ef9711595accda0b440083d50f851
kduckets <kmditroia@gmail.com>
2017-09-24 13:00:46 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -29,7 +29,8 @@ server.route({
   method: 'POST',
   path: '/',
   handler: function (request, reply) {
-    var fields = {RSVP:request.payload.RSVP, NAME:request.payload.NAME};
+    var fields = {RSVP:request.payload.RSVP, 
+      NAME:request.payload.NAME};
     console.log(JSON.stringify(fields));
     if (!request.payload.email) return reply({ detail: "Please provide a valid email address." }).code(400);
     if (!request.payload.list_id) return reply({ detail: "no_list_id" }).code(400);
